1140e201a60cd734800811645ae8ee8cd7a5507f
SteffonReed <112779982+SteffonReed@users.noreply.github.com>
2022-12-06 10:29:06 -0500

    Delete roulette_dealer_finder_by_time.sh

--- roulette_dealer_finder_by_time.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#shell script that can analyze future employee schedules to see which employee was working at a specific time for future losses.
-
-
-grep -i "$2 $3" $1_Dealer_schedule |awk -F" " '{print $1,$2,$3,$4,$5,$6,$7}'
-
